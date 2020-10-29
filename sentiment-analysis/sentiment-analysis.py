6f5e78b41dd349a7624e7a42e7127a6c222207e5
Nathaniel Flower <nathaniel@rewolf.co>
2020-10-29 17:14:57 -0400

    Updates

--- sentiment-analysis/sentiment-analysis.py
+++ sentiment-analysis/sentiment-analysis.py
@@ -1,15 +1,29 @@
 import csv
 import random
-import datetime
 import spacy
+import pickle
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
 from mlxtend.plotting import plot_confusion_matrix
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 import matplotlib.pyplot as plt
+from Data import DataSet, DataVersion, DataMatrix
+from sklearn.feature_extraction.text import CountVectorizer
 
+sentiment2int = {'negative': 0, 'neutral': 1, 'positive': 2}
+int2sentiment = list(sentiment2int.keys())
+airline2int = {'Virgin America': 0, 'United': 1, 'Southwest': 2,
+               'Delta': 3, 'US Airways': 4, 'American': 5}
+int2airline = list(airline2int.keys())
+
+emojis = ['😩', '🌞', '🇸', '🎀', '😀', '🙏', '👏', '💺', '😖', '😊', '😻', '✈', '👍', '🍸', '😎', '🎲', '✌', '💯',
+          '😘', '❄', '😁', '🙅', '💗', '🚪', '☀', '🎉', '😵', '°', '😃', '🇬', '💔', '✨', '☺', '🇧', '😆', '…', '😍',
+          '😫', '👌', '❤', '😢', '🌴', '💙', '👉', '💕', '🙌', '😱', '🌟', '☕', '📲', '😉', '👀', '😭', '👸', '😜',
+          '😂', ':)', ':(', ';)', ':-)', ':-(', ';-)', ':-/', ':/']
+###
+###
+###
 nlp = spacy.load('en_core_web_sm')
 
 sentiment2int = {'negative': 0, 'neutral': 1, 'positive': 2}
@@ -20,23 +34,16 @@ int2airline = list(airline2int.keys())
 
 tweets_by_airlines = [[[], [], []]] * len(int2airline)
 
-emojis = [
-    [':)'],
-    [':(']
-]
-
 rem_list = [
     "tweet_id", "negativereason", "negativereason_confidence", "airline_sentiment_gold", "negativereason_gold",
-    "tweet_coord", "tweet_location", "user_timezone", 'name', 'airline_sentiment_confidence'
+    "tweet_coord", "tweet_location", "user_timezone", 'name', 'airline_sentiment_confidence', 'text', 'lemma',
+    'airline', 'dep_bigram', 'airline_sentiment', 'isnegative', 'ispositive'
 ]
 
 def expand_features(tweet):
-    [tweet.pop(key) for key in rem_list]
-    tweet['airline'] = airline2int[tweet['airline']]
-    tweet['airline_sentiment'] = sentiment2int[tweet['airline_sentiment']]
-    tweet['retweet_count'] = int(tweet['retweet_count'])
-    tweet['tweet_created'] = datetime.datetime.strptime(tweet['tweet_created'], "%Y-%m-%d %H:%M:%S %z").timestamp()
     words = tweet['text'].split()
+    tweet['countvector'] = None
+    tweet['dep_bigram'] = []
     cleaned_words = []
     for word in words:
         if word[0] != '@':
@@ -48,68 +55,83 @@ def expand_features(tweet):
         if token.is_punct or token.like_url or not token.is_oov:
             continue
         tweet['lemma'].append(token.lemma_)
+        #tweet['dep_bigram'].append(f'{token.lemma_}{token.head.lemma_}')
+
+    for i in range(len(emojis)):
+        emoji = emojis[i]
+        if emoji in tweet['text']:
+            tweet['lemma'].append(f'-EMOJI{i}-')
     return tweet
 
-with open("Tweets.csv") as csvfile:
-    tweets = csv.DictReader(csvfile)
-    for row in tweets:
-        tweet = expand_features(row)
-        tweets_by_airlines[tweet['airline']][tweet['airline_sentiment']].append(row)
+train_filepath = 'datasets/v1/training-all.csv'
+train_cleantext = []
+train_target = []
+train_features = []
 
-# First set is training (50%), second is validation (25%), third is testing (25%)
-data = [[], []]
-cleaned_words = [[], []]
-for airline in tweets_by_airlines:
-    for tweet_set in airline:
-        i = 0
-        while tweet_set:
-            ind = random.randint(0, len(tweet_set)-1)
-            tweet = tweet_set.pop(ind)
-            cleaned_words[i % len(cleaned_words)].append(' '.join(tweet['lemma']))
-            data[i % len(data)].append(tweet)
-            i = (i + 1) % (len(data) + 1)
+with open(train_filepath, encoding='utf8') as csvfile:
+    training = csv.DictReader(csvfile)
+    for tweet in training:
+        training = expand_features(tweet)
+        train_cleantext.append(' '.join(tweet['lemma']) + ' ' + ' '.join(tweet['dep_bigram']))
+        train_target.append(tweet['airline_sentiment'])
+        train_features.append(tweet)
+
+validate_filepath = 'datasets/v1/validation-all.csv'
+valdidate_cleantext = []
+validate_target = []
+validate_features = []
+with open(validate_filepath, encoding='utf8') as csvfile:
+    validate = csv.DictReader(csvfile)
+    for tweet in validate:
+        validate = expand_features(tweet)
+        valdidate_cleantext.append(' '.join(tweet['lemma']) + ' ' + ' '.join(tweet['dep_bigram']))
+        validate_target.append(tweet['airline_sentiment'])
+        validate_features.append(tweet)
+
 
 v = CountVectorizer(analyzer = 'word')
-train_features = v.fit_transform(cleaned_words[0])
-test_features = v.transform(cleaned_words[1])
+train_vectors = v.fit_transform(train_cleantext)
+validate_vectors = v.transform(valdidate_cleantext)
 
-post_rem_list = ['text', 'lemma', 'tweet_created']
-final_train_features = []
-train_target = []
-for i in range(len(data[0])):
-    [data[0][i].pop(key) for key in post_rem_list]
-    train_target.append(data[0][i].pop('airline_sentiment'))
-    temp = []
-    temp.extend(data[0][i].values())
-    temp.extend(train_features[i].toarray()[0])
-    final_train_features.append(temp)
-
-final_test_features = []
-test_target = []
-for i in range(len(data[1])):
-    [data[1][i].pop(key) for key in post_rem_list]
-    test_target.append(data[1][i].pop('airline_sentiment'))
-    temp = []
-    temp.extend(data[1][i].values())
-    temp.extend(test_features[i].toarray()[0])
-    final_test_features.append(temp)
+for i in range(len(train_features)):
+    '''
+    if data[i][j]['isnegative']:
+        continue
+    '''
+    train_features[i] = train_vectors[i].toarray()[0].tolist()
+    #[train_features[i].pop(key) for key in rem_list]
 
 
-Classifiers = {
-    #DecisionTreeClassifier(),
-    #RandomForestClassifier(n_estimators=200)
-     }
+for i in range(len(validate_features)):
+    '''
+    if data[i][j]['isnegative']:
+        continue
+    '''
+    validate_features[i] = validate_vectors[i].toarray()[0].tolist()
+    #[validate_features[i].pop(key) for key in rem_list]
+
+###
+###
+###
+
+Classifiers = [
+    DecisionTreeClassifier(),
+    RandomForestClassifier(n_estimators=200)
+]
 
 Accuracy = []
 Model = []
 for classifier in Classifiers:
-    fit = classifier.fit(final_train_features,train_target)
-    pred = fit.predict(final_test_features)
-    accuracy = accuracy_score(pred,test_target)
-    Accuracy.append(accuracy)
+    fit = classifier.fit(train_features,train_target)
+    pred_train = fit.predict(train_features)
+    accuracy_train = accuracy_score(pred_train,train_target)
+    pred_val = fit.predict(validate_features)
+    accuracy_val = accuracy_score(pred_val,validate_target)
+    Accuracy.append(accuracy_val)
     Model.append(classifier.__class__.__name__)
-    print('Accuracy of '+classifier.__class__.__name__+' is '+str(accuracy))
-    print(classification_report(pred,test_target))
+    print('Accuracy training of '+classifier.__class__.__name__+' is '+str(accuracy_train))
+    print('Accuracy validation of '+classifier.__class__.__name__+' is '+str(accuracy_val))
+    print(classification_report(pred_val,validate_target))
     '''
     cm=confusion_matrix(pred , test_target)
     plt.figure()
